c322fb6808be80328e01853ba854b85a145a2717
Gustavo Rosas Martinez <goldem64@yahoo.com.mx>
2016-05-02 00:22:10 -0500

    agregando comentarios

--- BTC02/archivos.py
+++ BTC02/archivos.py
@@ -1,24 +1,27 @@
+# con la funcion 'open' podemos abrir un archivo, no hay que olvidar indicar la extension del archivo
 file = open("archivo1.txt")
-
+# el metodo readline() permite almacenar una linea del archivo que estamos abriendo
 linea = file.readline()
 
+# este codigo nos permite procesar el archivo linea por linea e imprimirla
 while linea != "":
 	print (linea)
 	linea = file.readline()
+# cerramos el archivo
+file.close()
 
-print ("Hola")
-print ("Mundo")
-
-
+# otra forma de procesar un archivo es utilizando un for e iterar el archivo, de esta forma iteramos el archivo por linea
 print ("Procesando archivo linea por linea con for")
 
-file.close()
+
 
 archivo = open ("archivo1.txt")
 for linea in archivo:
 	print (linea, end="")
 
 archivo.close()
+
+# una segunda forma de procesar el archivo es procesando la totalidad del archivo con la funcion  readlines
 
 file = open("archivo1.txt")
 
